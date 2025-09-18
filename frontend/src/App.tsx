5666e5116ce06b46794db0ceda9b7456fd780605
maurodk <131914511+maurodk@users.noreply.github.com>
2025-09-18 10:33:50 -0300

    ajustes mobile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -753,7 +753,10 @@ function App() {
     <HelmetProvider>
       <Helmet>
         <title>Implantação Digital - VCA CONSTRUTORA</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes"
+        />
       </Helmet>
 
       <div className={`page-wrapper ${isMappingMode ? "sidebar-visible" : ""}`}>
