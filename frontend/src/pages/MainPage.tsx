6ccc50a99533c774132ccc08b8522c27ebc0a254
maurodk <131914511+maurodk@users.noreply.github.com>
2025-11-03 12:30:16 -0300

    Adjust BUILD

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,7 +1,6 @@
 // src/pages/MainPage.tsx
 
 import { useState, useEffect, useMemo, useRef, useCallback } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import { Helmet, HelmetProvider } from "@dr.pogodin/react-helmet";
 import { useReactToPrint } from "react-to-print";
